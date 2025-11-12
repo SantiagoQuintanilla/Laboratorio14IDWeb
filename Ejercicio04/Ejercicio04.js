bcbacfb64eab9022a92611708abd571857620f80
SantiagoQuintanilla <squintanilla@unsa.edu.pe>
2025-11-12 18:36:33 -0500

    Finalizado Ejercicio 4

---
+++ Ejercicio04/Ejercicio04.js
@@ -0,0 +1,5 @@
+let numero=parseInt(prompt("->Ingrese el número a operar: "));
+const cuadrado=Math.pow(numero, 2);
+const cubo=Math.pow(numero, 3);
+const raiz=Math.sqrt(numero);
+alert(`->Cuadrado de ${numero} = ${cuadrado}\n->Cubo de ${numero} = ${cubo}\n->Raiz cuadrada de ${numero} = ${raiz}` );
