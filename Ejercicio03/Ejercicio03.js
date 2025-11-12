bd9fff593bc431e83545220c4a9f371f00e9674e
SantiagoQuintanilla <squintanilla@unsa.edu.pe>
2025-11-12 18:39:53 -0500

    Correccion, Finalizado Ejercicio 3

--- Ejercicio03/Ejercicio03.js
+++ Ejercicio03/Ejercicio03.js
@@ -2,9 +2,7 @@ function numeroAleatorio(min, max){
     const numAleatorio=(Math.random()*max-min)+min;
     return numAleatorio;
 }
-for (let i=0; i<20; i++){
 let dado1=Math.round(numeroAleatorio(1, 6));
 let dado2=Math.round(numeroAleatorio(1, 6)); 
 let suma=dado1+dado2;
 alert(`-Suma de lanzamiento de dados: ${dado1} + ${dado2} : ${suma}`);
-}
